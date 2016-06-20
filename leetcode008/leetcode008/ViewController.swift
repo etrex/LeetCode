b38cbb67d6cc55b0babb14494d01f47442665be2
etrex kuo <et284vu065k3@gmail.com>
2016-06-20 14:10:39 +0800

    leetcode 008 String to Integer (atoi)

---
+++ leetcode008/leetcode008/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  leetcode008
+//
+//  Created by etrex kuo on 2016/6/20.
+//  Copyright © 2016年 etrex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var i = Solution().myAtoi("000100")
+        print(i)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
